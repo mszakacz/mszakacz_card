c7f9963b349ddaed14791f0ca2a37187b30d16cf
Maciej <mszakacz@gmail.com>
2022-03-02 09:55:41 +0100

    fix(lib): Improve window model with equatable, copyWith and props

--- lib/models/window.dart
+++ lib/models/window.dart
@@ -1,16 +1,43 @@
-import '../consts/position.dart';
-import '../consts/dimensions.dart';
+import 'package:equatable/equatable.dart';
+import 'package:mszakacz_card/constant/position.dart';
 
-class Window {
+class Window extends Equatable {
   const Window({
     required this.position,
     required this.top,
     required this.left,
     required this.size,
+    required this.selected,
   });
 
   final Position position;
   final double top;
   final double left;
   final double size;
+  final bool selected;
+
+  Window copyWith({
+    Position? position,
+    double? top,
+    double? left,
+    double? size,
+    bool? selected,
+  }) {
+    return Window(
+      position: position ?? this.position,
+      top: top ?? this.top,
+      left: left ?? this.left,
+      size: size ?? this.size,
+      selected: selected ?? this.selected,
+    );
+  }
+
+  @override
+  List<Object> get props => [
+        position,
+        top,
+        left,
+        size,
+        selected,
+      ];
 }
