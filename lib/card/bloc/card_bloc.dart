3fb9ba829074ab7809c982c18b759a11ca17ef89
Maciej <mszakacz@gmail.com>
2022-03-02 08:42:52 +0100

    feat(card): Add CardBloc

---
+++ lib/card/bloc/card_bloc.dart
@@ -0,0 +1,64 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:mszakacz_card/consts/dimensions.dart';
+import '../../models/window.dart';
+import 'package:mszakacz_card/consts/position.dart';
+
+part 'card_event.dart';
+part 'card_state.dart';
+
+class CardBloc extends Bloc<CardEvent, CardState> {
+  CardBloc()
+      : super(
+          CardState(
+            windows: [
+              Window(
+                position: Position.largeBotRight,
+                top: botLargeWindowTop,
+                left: rightLargeWindowLeft,
+                size: largeWindowSize,
+                selected: true,
+              ),
+              Window(
+                position: Position.botLeft,
+                top: botRowPositionTop,
+                left: leftColumnPositionLeft,
+                size: smallWindowSize,
+                selected: false,
+              ),
+              Window(
+                position: Position.midLeft,
+                top: midRowPositionTop,
+                left: leftColumnPositionLeft,
+                size: smallWindowSize,
+                selected: false,
+              ),
+              Window(
+                position: Position.topLeft,
+                top: topRowPositionTop,
+                left: leftColumnPositionLeft,
+                size: smallWindowSize,
+                selected: false,
+              ),
+              Window(
+                position: Position.topCenter,
+                top: topRowPositionTop,
+                left: midColumnPositionLeft,
+                size: smallWindowSize,
+                selected: false,
+              ),
+              Window(
+                position: Position.topRight,
+                top: topRowPositionTop,
+                left: rightColumnPositionLeft,
+                size: smallWindowSize,
+                selected: false,
+              ),
+            ],
+          ),
+        ) {
+    on<CardEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
