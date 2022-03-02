68d5c8e0b88347ac4b7962e6260622a786b9bd45
Maciej <mszakacz@gmail.com>
2022-03-02 21:39:42 +0100

    fix(lib): remove position from the window model properties

--- lib/models/window.dart
+++ lib/models/window.dart
@@ -1,30 +1,25 @@
 import 'package:equatable/equatable.dart';
-import 'package:mszakacz_card/constant/position.dart';
 
 class Window extends Equatable {
   const Window({
-    required this.position,
     required this.top,
     required this.left,
     required this.size,
     required this.selected,
   });
 
-  final Position position;
   final double top;
   final double left;
   final double size;
   final bool selected;
 
   Window copyWith({
-    Position? position,
     double? top,
     double? left,
     double? size,
     bool? selected,
   }) {
     return Window(
-      position: position ?? this.position,
       top: top ?? this.top,
       left: left ?? this.left,
       size: size ?? this.size,
@@ -34,7 +29,6 @@ class Window extends Equatable {
 
   @override
   List<Object> get props => [
-        position,
         top,
         left,
         size,
