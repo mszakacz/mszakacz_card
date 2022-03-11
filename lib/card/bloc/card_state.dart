2ae16d718ac2263941de5b74fc52fd2770272a84
Maciej <mszakacz@gmail.com>
2022-03-11 07:31:49 +0100

    refactor(card): change the property of CardState to List of WindowPosition (enum) instead of windows (list of obejcts with dimensions)

--- lib/card/bloc/card_state.dart
+++ lib/card/bloc/card_state.dart
@@ -3,18 +3,22 @@ part of 'card_bloc.dart';
 class CardState extends Equatable {
   const CardState({
     required this.windows,
+    required this.selected,
   });
 
-  final List<Window> windows;
+  final List<WindowPosition> windows;
+  final int selected;
 
   @override
-  List<Object> get props => [windows];
+  List<Object> get props => [windows, selected];
 
   CardState copyWith({
-    List<Window>? windows,
+    List<WindowPosition>? windows,
+    int? selected,
   }) {
     return CardState(
       windows: windows ?? this.windows,
+      selected: selected ?? this.selected,
     );
   }
 }
