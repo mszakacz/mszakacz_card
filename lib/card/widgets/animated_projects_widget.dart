cfbe1c1db128f7da03e6bafe785f435e0fab0db4
Maciej <mszakacz@gmail.com>
2022-03-23 13:33:17 +0100

    feat: add proper url to projects, add Flutte logo

--- lib/card/widgets/animated_projects_widget.dart
+++ lib/card/widgets/animated_projects_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mszakacz_card/constant/exceptions.dart';
 import 'package:mszakacz_card/l10n/l10n.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -20,82 +21,147 @@ class AnimatedProjectsWidget extends StatelessWidget {
           begin: Alignment.topRight,
           end: Alignment.bottomLeft,
           stops: [
-            0.2,
-            0.8,
+            0.1,
+            0.9,
           ],
           colors: [
-            Color.fromARGB(255, 240, 236, 32),
-            Color.fromARGB(255, 218, 148, 20),
+            Colors.orange,
+            Color.fromARGB(255, 255, 216, 88),
           ],
         ),
       ),
-      child: AnimatedSwitcher(
-        duration: const Duration(milliseconds: 500),
-        child: selected
-            ? const Center(
-                child: Text(
-                  'Projects',
-                  style: TextStyle(
-                    fontSize: 22,
-                    fontFamily: 'Agne',
-                    color: Color.fromARGB(255, 23, 14, 145),
-                  ),
+      child: Stack(
+        children: [
+          AnimatedOpacity(
+            duration: const Duration(milliseconds: 500),
+            opacity: selected ? 0.0 : 1.0,
+            child: Center(
+              child: Text(
+                l10n.animatedProjectsWidgetName,
+                style: const TextStyle(
+                  fontSize: 22,
+                  color: Color.fromARGB(255, 23, 14, 145),
                 ),
-              )
-            : Center(
+              ),
+            ),
+          ),
+          IgnorePointer(
+            ignoring: !selected,
+            child: AnimatedOpacity(
+              opacity: selected ? 1.0 : 0.0,
+              duration: const Duration(milliseconds: 500),
+              child: Center(
                 child: DefaultTextStyle(
                   style: const TextStyle(
                     fontSize: 16,
-                    fontFamily: 'Bobbers',
-                    color: Color.fromARGB(255, 15, 8, 53),
+                    color: Color.fromARGB(255, 9, 30, 146),
                   ),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
+                      // Project 1 - Portfolio
+
                       GestureDetector(
                         onTap: () async {
-                          const _url = 'https://better-organizer.web.app/#/';
+                          const _url =
+                              'https://github.com/mszakacz/mszakacz_card#readme';
                           if (!await launch(
                             _url,
-                          )) throw 'Could not launch $_url';
+                          )) throw LaunchUrlFailure();
                         },
                         child: FittedBox(
                           fit: BoxFit.fitWidth,
-                          child: Text(l10n.animatedProjectsWidgetText1),
+                          child: Row(
+                            children: [
+                              const FlutterLogo(),
+                              const SizedBox(width: 10),
+                              Text(
+                                l10n.animatedProjectsWidgetText4,
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                       const SizedBox(height: 15),
+
+                      // Project 2
+
                       GestureDetector(
                         onTap: () async {
-                          const _url = 'https://github.com/mszakacz/user-list';
+                          const _url =
+                              'https://github.com/mszakacz/better_organizer#readme';
                           if (!await launch(
                             _url,
-                          )) throw 'Could not launch $_url';
+                          )) throw LaunchUrlFailure();
                         },
                         child: FittedBox(
                           fit: BoxFit.fitWidth,
-                          child: Text(l10n.animatedProjectsWidgetText2),
+                          child: Row(
+                            children: [
+                              const FlutterLogo(),
+                              const SizedBox(width: 10),
+                              Text(
+                                l10n.animatedProjectsWidgetText1,
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                       const SizedBox(height: 15),
                       GestureDetector(
                         onTap: () async {
                           const _url =
-                              'https://github.com/mszakacz/flutter-number-to-word';
+                              'https://github.com/mszakacz/user-list#readme';
                           if (!await launch(
                             _url,
-                          )) throw 'Could not launch $_url';
+                          )) throw LaunchUrlFailure();
                         },
                         child: FittedBox(
                           fit: BoxFit.fitWidth,
-                          child: Text(l10n.animatedProjectsWidgetText3),
+                          child: Row(
+                            children: [
+                              const FlutterLogo(),
+                              const SizedBox(width: 10),
+                              Text(
+                                l10n.animatedProjectsWidgetText2,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 15),
+
+                      // Project 3
+
+                      GestureDetector(
+                        onTap: () async {
+                          const _url =
+                              'https://github.com/mszakacz/flutter-number-to-word#readme';
+                          if (!await launch(
+                            _url,
+                          )) throw LaunchUrlFailure();
+                        },
+                        child: FittedBox(
+                          fit: BoxFit.fitWidth,
+                          child: Row(
+                            children: [
+                              const FlutterLogo(),
+                              const SizedBox(width: 10),
+                              Text(
+                                l10n.animatedProjectsWidgetText3,
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ],
                   ),
                 ),
               ),
+            ),
+          ),
+        ],
       ),
     );
   }
